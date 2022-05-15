deb549fa2328c51a555f806e1d20e76dd215be55
Pystoi228 <104297096+Pystoi228@users.noreply.github.com>
2022-05-15 11:02:42 +0300

    Add files via upload

---
+++ ping pong.py
@@ -0,0 +1,59 @@
+from pygame import *
+from random import *
+from time import time as timer
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+fon = (200,225, 200)
+window = display.set_mode((700, 500))
+window.fill(fon)
+
+clock = time.Clock()
+FPS = 60
+
+
+
+
+
+lost = 0
+toto = 0
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        if self.rect.y == 450:
+            self.rect.y = 0
+            self.rect.x = randint(0, 600)
+            self.speed = randint(1, 3)
+            lost = lost + 1
+#hero = Player('rocket.png', 200, 400, 1, 60, 60)
+
+#hero.apdate()
+#hero.reset()
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+                
+
+
+    
+
+
+   
+    display.update()
+    clock.tick(FPS)
+
+
+
+    
+    
